0caad89eec1505445680cae499c4035c09201536
Allan Burdajewicz <allan.burdajewicz@gmail.com>
2019-09-23 21:02:05 +1000

    DinD: Rely on the official image entrypoint

--- agents/dind-aws/dind-agent-entrypoint.sh
+++ agents/dind-aws/dind-agent-entrypoint.sh
@@ -2,28 +2,18 @@
 # Optional environment variables :
 # * DOCKER_OPTS : Docker options to add to the docker daemon
 
-set -e
+set -eu
 
-if [ "$#" -eq 0 ]; then
-    # See https://github.com/docker-library/docker/blob/master/18.09/dind/dockerd-entrypoint.sh
-    set -- dockerd \
-		    --host=unix:///var/run/docker.sock \
-		    --host=tcp://0.0.0.0:2375 \
-		    "$DOCKER_OPTS"
-else 
-    # See https://github.com/mesosphere/dcos-jenkins-dind-agent/blob/master/wrapper.sh
-    echo "Starting the Docker daemon..."
-    sh "$(which dind)" dockerd \
-        --host=unix:///var/run/docker.sock \
-        --host=tcp://0.0.0.0:2375 \
-        $DOCKER_OPTS &
+DOCKER_OPTS="${DOCKER_OPTS:-}"
+
+echo "Starting the Docker daemon..."
+/usr/local/bin/dockerd-entrypoint.sh ${DOCKER_OPTS} >/docker.log 2>&1 &
     
-    while(! docker info > /dev/null 2>&1); do
-        echo "Waiting for the Docker daemon to start..."
-        sleep 1
-    done
-    
-    echo "Docker Daemon started"
-fi
-    
+while(! docker info > /dev/null 2>&1); do
+  echo "Waiting for the Docker daemon to start..."
+  sleep 1
+done
+
+echo "Docker Daemon started"
+
 exec "$@"
